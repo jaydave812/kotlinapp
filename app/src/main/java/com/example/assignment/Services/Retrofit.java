591cd966e265e3522edbfd581ccb0009dbe01550
Harshil Shah <shahharshil0@yahoo.com>
2020-01-27 00:13:15 +0530

    logging completed

--- app/src/main/java/com/example/assignment/Services/Retrofit.java
+++ app/src/main/java/com/example/assignment/Services/Retrofit.java
@@ -1,34 +1,43 @@
 package com.example.assignment.Services;
 
-import com.example.assignment.Utils.AppConstant;
+import com.example.assignment.BuildConfig;
 
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Retrofit {
-    public static Api_Services api_services = null;
+    private static Api_Services api_services = null;
+
+    private static OkHttpClient provideOkHttpClient(HttpLoggingInterceptor interceptor) {
+        OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+        okHttpClient.addInterceptor(interceptor);
+        okHttpClient.connectTimeout(180, TimeUnit.SECONDS)
+                .writeTimeout(180, TimeUnit.SECONDS)
+                .readTimeout(180, TimeUnit.SECONDS);
+        return okHttpClient.build();
+    }
+
+    private static HttpLoggingInterceptor provideLoggingInterceptor() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.level(HttpLoggingInterceptor.Level.BODY);
+        return interceptor;
+    }
 
     public static Api_Services getApi_services() {
         if (api_services == null) {
-            retrofit2.Retrofit retrofit = new retrofit2.Retrofit.Builder().client(okHttpClient)
-                    .baseUrl(AppConstant.BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+            retrofit2.Retrofit retrofit =
+                    new retrofit2.Retrofit.Builder()
+                            .baseUrl(BuildConfig.BASE_URL)
+                            .client(Retrofit.provideOkHttpClient(provideLoggingInterceptor()))
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
 
             api_services = retrofit.create(Api_Services.class);
         }
         return api_services;
     }
-
-
-
-
-    final static OkHttpClient okHttpClient = new OkHttpClient.Builder()
-            .connectTimeout(180, TimeUnit.SECONDS)
-            .writeTimeout(180, TimeUnit.SECONDS)
-            .readTimeout(180, TimeUnit.SECONDS)
-            .build();
 
 }
